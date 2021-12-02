27770131be59b25ed458fc8310710e655473fabe
Iftee1711402 <tawsifhaque97@gmail.com>
2021-12-03 04:51:06 +0600

    Updates

---
+++ precache-manifest.8e2343fbcf06999d299f781273a47855.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1f2c8b1f73d7ed13f9962e5e8517fe",
+    "url": "/todo_list_react/index.html"
+  },
+  {
+    "revision": "0fd7d30f77bc6c408602",
+    "url": "/todo_list_react/static/js/2.8ee70edf.chunk.js"
+  },
+  {
+    "revision": "38dbf5a05b60ca4f596b",
+    "url": "/todo_list_react/static/js/main.0b588d1c.chunk.js"
+  },
+  {
+    "revision": "3461a94f363cf62f22a9",
+    "url": "/todo_list_react/static/js/runtime-main.d981949a.js"
+  }
+]);
